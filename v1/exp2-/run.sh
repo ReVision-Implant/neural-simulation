b0f09f52ddbbe7e3ac9faf0deb3ea994b3dbee1a
nilsvanrompaey <nils.vanrompaey@student.kuleuven.be>
2023-08-14 15:12:33 +0200

    Deleted files

--- v1/exp2-/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source /users/students/r0754386/Documents/bmtk/bin/activate
-module load mpi
-cd /users/students/r0754386/Documents/bmtk/examples/v1
-mpirun -np 12 nrniv -mpi -python run_bionet.py exp2-/config/config_xstim.json
-
-
--- v1/exp3-/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source /users/students/r0754386/Documents/bmtk/bin/activate
-module load mpi
-cd /users/students/r0754386/Documents/bmtk/examples/v1
-python run_bionet.py exp3-/config/1/-/10/config_01-_10_0.json
-
-
-
