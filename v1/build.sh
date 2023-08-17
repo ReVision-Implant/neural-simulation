22f04d37de7116e3711fb5d629049799ee225f8c
nilsvanrompaey <nils.vanrompaey@student.kuleuven.be>
2023-08-17 15:25:45 +0200

    restore to original

--- v1/build.sh
+++ v1/build.sh
@@ -6,7 +6,7 @@
 module load mpi
 cd "${0%/*}"        # Change the working dir to where this script is located
 
-mpirun -np 1 python build_network.py --fraction 0.001 -o networks_25/network0 --rng-seed 100  # !!! Change the arguments according to your needs !!!
+mpirun -np 12 python build_network.py --fraction 0.001 -o networks_25/network0 --rng-seed 100  # !!! Change the arguments according to your needs !!!
 # You can add as many lines as you want, e.g.
 # mpirun -np 12 python build_network.py --fraction 0.25 -o networks_25/network1 --rng-seed 101
 # mpirun -np 12 python build_network.py --fraction 0.25 -o networks_25/network2 --rng-seed 102
