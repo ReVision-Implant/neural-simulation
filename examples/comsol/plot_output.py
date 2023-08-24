36d7cf784973f067b430f25d5caec46d9e0952c5
nilsvanrompaey <nils.vanrompaey@student.kuleuven.be>
2023-08-24 16:48:04 +0200

    Update comsol example

--- examples/comsol/plot_output.py
+++ examples/comsol/plot_output.py
@@ -7,8 +7,13 @@ sys.path.append('../bio_components')
 from bmtk.analyzer.compartment import plot_traces
 from bmtk.analyzer.spike_trains import plot_raster, plot_rates_boxplot
 
-plot_traces(config_file='config.comsol_tdep.json', report_name='membrane_potential')
+# plot_raster(config_file='config.comsol_tdep.json')
+# plot_traces(config_file='config.comsol_tdep.json', report_name='membrane_potential')
+
+plot_raster(config_file='config.comsol_stat.json')
 plot_traces(config_file='config.comsol_stat.json', report_name='membrane_potential')
+
+plot_raster(config_file='config.comsol_stat2.json')
 plot_traces(config_file='config.comsol_stat2.json', report_name='membrane_potential')
 
 
