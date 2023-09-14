502d229b7280f303bc2a438d181d4bc72e89168f
nilsvanrompaey <nils.vanrompaey@student.kuleuven.be>
2023-09-14 17:10:32 +0200

    docs and small change to get_rotations

--- v1_Anke/components/hdf5.py
+++ v1_Anke/components/hdf5.py
@@ -21,7 +21,8 @@ class HDF5:
         self.file = h5py.File(self.dir, 'r+')
         self.v1 = v1
         self.get_positions()
-        self.get_rotations()
+        rot = self.get_rotations()
+        print(rot)
         if plot:
             self.plot_positions(labels=['X','Z','Y'])
         
@@ -59,10 +60,12 @@ class HDF5:
         :return: node rotations
         :rtype: ndarray
         """
+        ### Initialise
         self.x_rot = None
         self.y_rot = None
         self.z_rot = None
 
+        ### If rotation angles exist, set as attributes
         if 'rotation_angle_xaxis' in self.file['nodes'][self.name]['0'].keys():
             self.x_rot = self.file['nodes'][self.name]['0']['rotation_angle_xaxis'][:]
         if 'rotation_angle_yaxis' in self.file['nodes'][self.name]['0'].keys():
@@ -70,18 +73,23 @@ class HDF5:
         if 'rotation_angle_zaxis' in self.file['nodes'][self.name]['0'].keys():
             self.z_rot = self.file['nodes'][self.name]['0']['rotation_angle_zaxis'][:]
         
-        # self.rotations = np.vstack((self.x_rot, self.y_rot, self.z_rot)).T
+        ### Create dictionary of present rotation angles {'axis_rot':self.axis_rot}
+        self.rotations = {a:getattr(self, a) for a in ['x_rot', 'y_rot', 'z_rot'] if hasattr(self,a)}
+        self.rotations = {k:v for k, v in self.rotations.items() if v is not None}
 
-        return
+        return self.rotations
 
     def plot_positions(self):
         """Plot node positions.
-        """            
+        """
+        ### Create 1x2 subplot    
         _, ax = plt.subplots(1,2,figsize=(9,12), subplot_kw={'projection':'3d'})
         
+        ### Plot first perspective
         self.subplot(ax[0], 'YZ')
         ax[0].view_init(elev=0, azim=0)
 
+        ### Plot second perspective
         self.subplot(ax[1], 'XY')
         ax[1].view_init(elev=90., azim=270)
         plt.show()
@@ -94,6 +102,7 @@ class HDF5:
         :param title: subplot title.
         :type title: str
         """
+        ### Set attributes
         ax.scatter(self.x_pos,self.y_pos,self.z_pos, marker='.', s=3)
         ax.set_title(title)
         ax.set_xlabel('X')
@@ -101,9 +110,13 @@ class HDF5:
         ax.set_zlabel('Z')
 
     def convert_pos_to_v1(self):
+        """Convert 'positions' dataset into three datasets 'x', 'y', 'z' in nodes/name/0 for use in VND.
+        """
+        ### If nodes are already in v1 format, do nothing
         if self.v1:
             pass
-        else:
+        ### Else, try creating new datasets 'x', 'y', 'z' or overwrite if they already exist
+        else: 
             try:
                 self.file.create_dataset('nodes/'+self.name+'/0/x', data=self.x_pos)
                 self.file.create_dataset('nodes/'+self.name+'/0/y', data=self.y_pos)
@@ -118,26 +131,31 @@ class HDF5:
     
 
 class SpikeScaler():
-    """Helper class to scale spike timings for use in VND.
+    """Scale spike timings for use in VND.
     """
     def __init__(self, path, scale):
-        """Read .h5 file and call main functions.
+        """Read .h5 file and call main function.
 
-        :param dir: path to .h5 file.
-        :type dir: path
+        :param path: Path to .h5 file.
+        :type path: path
+        :param scale: Scale factor for the spike timings. The time axis is multiplied by this scale factor.
+        :type scale: int
         """
+        ### Set attributes
         self.source = os.path.abspath(__file__) + '/../../../' + path
         self.dest = os.path.dirname(self.source)+"/spikes_scaled.h5"
         self.scale = scale
         self.scale_spike_timings()
 
     def scale_spike_timings(self):
-
+        """Copies the original spikes.h5 and changes the time scale in the copy.
+        """
+        ### Copy original file into scaled file
         self.orig_file = h5py.File(self.source, 'r')
         self.scaled_file = h5py.File(self.dest, 'w')
-
         self.scaled_file.copy(self.orig_file['spikes'], 'spikes')
-
+        
+        ### Scale timestamps 
         for i in list(self.scaled_file['spikes'].keys()):
             timestamps = self.scaled_file['spikes/'+i+'/timestamps'][:]
             del self.scaled_file['spikes/'+i+'/timestamps']
@@ -148,14 +166,13 @@ class SpikeScaler():
             units = self.orig_file['spikes/'+i+'/timestamps'].attrs.__getitem__('units')
             self.scaled_file['spikes/'+i+'/timestamps'].attrs.__setitem__('units', units)
 
-
+        ### Close both files
         self.orig_file.close()
         self.scaled_file.close()
+        
         return
 
 if __name__ == "__main__":
     root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
     hf = HDF5(root + '/examples/comsol/network/column_nodes.h5')
-    hf.convert_to_v1()
-
-### !!!Copy whole spikes group to newly created h5!!!
+    hf.convert_pos_to_v1()
