e65cdbd0e006a89467b070c0de977c02c5503c1e
nilsvanrompaey <nils.vanrompaey@student.kuleuven.be>
2023-09-18 13:30:32 +0200

    Move python files: 'components' -> 'toolbox'

--- v1_Anke/toolbox/animate.py
+++ v1_Anke/toolbox/animate.py
@@ -3,12 +3,12 @@ from hdf5 import HDF5
 import matplotlib.pyplot as plt
 import matplotlib
 import numpy as np
-from matplotlib.animation import FuncAnimation
+import matplotlib.animation
 import mpl_toolkits.axes_grid1
 import matplotlib.widgets
 from file_helper import get_dirs
 
-class Player(FuncAnimation):
+class Player(matplotlib.animation.FuncAnimation):
     def __init__(self, fig, func, frames=None, init_func=None, fargs=None,
                  save_count=None, mint=0, maxt=100, pos=(0.125, 0.92), interval=5, **kwargs):
         self.t = 0
@@ -20,7 +20,7 @@ class Player(FuncAnimation):
         self.fig = fig
         self.func = func
         self.setup(pos)
-        FuncAnimation.__init__(self,self.fig, self.update, frames=self.play(), 
+        matplotlib.animation.FuncAnimation.__init__(self,self.fig, self.update, frames=self.play(), 
                                            init_func=init_func, fargs=fargs,
                                            save_count=save_count, interval=int(20*interval), **kwargs, )    
 
@@ -94,44 +94,47 @@ class Player(FuncAnimation):
     def update(self,t):
         self.slider.set_val(5*t)
 
-fig, ax = plt.subplots(figsize=(6,6))
-scat = ax.scatter([], [], c="b", s=5,)
-ax.set_aspect('equal','box')
-ax.set(xlim=[-200,200], ylim=[-200,200], xlabel='x [um]', ylabel='y [um]')
-ax.legend()
 
-dictionary = get_dirs(0, 0, 30, [1,2])
-nodes_dir = dictionary["nodes_dirs"][0]
-spikes_dir = dictionary["spikes_dirs"][0]
+if __name__ == "__main__":
 
-v1 = True
-node_pos = HDF5(nodes_dir, v1=True).get_positions(v1=True)
+    fig, ax = plt.subplots(figsize=(6,6))
+    scat = ax.scatter([], [], c="b", s=5,)
+    ax.set_aspect('equal','box')
+    ax.set(xlim=[-200,200], ylim=[-200,200], xlabel='x [um]', ylabel='y [um]')
+    ax.legend()
 
-spikes = pd.read_csv(spikes_dir, sep='\s+')
-timestamps = spikes["timestamps"]
-node_ids = spikes["node_ids"]
-positions = node_pos[node_ids]
+    dictionary = get_dirs(0, 0, 30, [1,2])
+    nodes_dir = dictionary["nodes_dirs"][0]
+    spikes_dir = dictionary["spikes_dirs"][0]
 
-circle = positions[:,0]**2 + positions[:,2]**2 <200**2
-positions = positions[circle]
-timestamps = timestamps[circle]
+    v1 = True
+    node_pos = HDF5(nodes_dir, v1=True).get_positions(v1=True)
 
-interval=5
-t_max = 100
-duration = 10000
-frames = int(duration/interval)
+    spikes = pd.read_csv(spikes_dir, sep='\s+')
+    timestamps = spikes["timestamps"]
+    node_ids = spikes["node_ids"]
+    positions = node_pos[node_ids]
 
-def update(t):
-    # for each frame, update the data stored on each artist.
-    # a = (timestamps<=t)*(timestamps>(t-t_max/frames))
-    a = (timestamps<=t*interval)
-    x = positions[a,0]
-    z = positions[a,2]
-    # update the scatter plot:
-    data = np.stack([x, z]).T
-    scat.set_offsets(data)
-    return scat
+    circle = positions[:,0]**2 + positions[:,2]**2 <200**2
+    positions = positions[circle]
+    timestamps = timestamps[circle]
 
-ani = Player(fig, update, mint=0, maxt=20, interval=interval)
+    interval=5
+    t_max = 100
+    duration = 10000
+    frames = int(duration/interval)
 
-plt.show()
+    def update(t):
+        # for each frame, update the data stored on each artist.
+        # a = (timestamps<=t)*(timestamps>(t-t_max/frames))
+        a = (timestamps<=t*interval)
+        x = positions[a,0]
+        z = positions[a,2]
+        # update the scatter plot:
+        data = np.stack([x, z]).T
+        scat.set_offsets(data)
+        return scat
+
+    ani = Player(fig, update, mint=0, maxt=20, interval=interval)
+
+    plt.show()
