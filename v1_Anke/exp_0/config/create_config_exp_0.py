90a7aa72cf1e9284be0168edb959f57441aca229
ankevge <anke.verhaege@student.kuleuven.be>
2023-11-30 10:21:57 +0100

    config files exp 0

--- v1_Anke/exp_0/config/create_config_exp_0.py
+++ v1_Anke/exp_0/config/create_config_exp_0.py
@@ -4,4 +4,4 @@ sys.path.append(module_path);
 
 from file_helper import create_configs;
 
-create_configs('./template.json',0, [0], [10], [0], overwrite=True);
+create_configs('./template.json',0, [0,1,2,3], [10,20], [0,1,2], overwrite=True);
