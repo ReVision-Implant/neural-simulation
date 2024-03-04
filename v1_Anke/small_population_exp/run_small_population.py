2ec1430db0f01dd9a7d134b9626e8e5a3bf8b61b
ankevge <anke.verhaege@gmail.com>
2024-03-04 17:01:22 +0100

    hyperstim notebook

--- v1_Anke/small_population_exp/run_small_population.py
+++ v1_Anke/small_population_exp/run_small_population.py
@@ -1,8 +1,8 @@
 from bmtk.simulator import bionet
 
 
-#conf = bionet.Config.from_json('simulation/config.json')
-conf=bionet.Config.from_json('simulation_long_axons/config.json')
+conf = bionet.Config.from_json('simulation/config.json')
+#conf=bionet.Config.from_json('simulation_long_axons/config.json')
 #conf=bionet.Config.from_json(simulation_directed_axons/config.json')
 conf.build_env()
 net = bionet.BioNetwork.from_config(conf)
