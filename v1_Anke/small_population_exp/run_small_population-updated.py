5ccb58d3c493cbf550f88f56ec5989409bb680dd
ankevge <anke.verhaege@gmail.com>
2024-03-11 10:58:20 +0100

    updated run file small model Kael Allen Institute

---
+++ v1_Anke/small_population_exp/run_small_population-updated.py
@@ -0,0 +1,67 @@
+from bmtk.simulator import bionet
+
+from bmtk.simulator.bionet.pyfunction_cache import add_cell_processor
+from bmtk.simulator.bionet.default_setters.cell_models import fix_axon_peri, set_params_peri
+from bmtk.simulator.bionet.io_tools import io
+from neuron import h
+
+
+def fix_axon_peri_multiple_stubs(hobj, num_stubs, stub_lengths, stub_diameters):
+    """
+    Replace reconstructed axon with multiple stubs.
+    :param hobj: hoc object
+    :param num_stubs: Number of stubs to create
+    :param stub_lengths: list of lenghts for the stubs
+    :param stub_diameters 
+    """
+
+    #Delete existing axon sections
+    for sec in hobj.axon:
+        h.delete_section(sec=sec)
+
+    #Create new axon structure with specified number of stubs
+    h.execute(f'create axon[{num_stubs}]', hobj)
+
+    #Set properties for each stub
+    for i in range (num_stubs):
+        hobj.axon[i].L=stub_lengths[i]
+        hobj.axon[i].diam=stub_diameters[i]
+        hobj.axonal.append(sec=hobj.axon[i])
+        hobj.all.append(sec=hobj.axon[i])
+
+    #Connect axon sections
+    for i in range (num_stubs-1):
+        hobj.axon[i+1].connect(hobj.axon[i],1,0) #first parameter 1 = where section is connected; value 1 means the connection is made at the end of the section; which is the distal end of the section being connected to, second parameter: connection is made at the proximal end of the section being connected to
+
+    #connect the first stub to the soma
+    hobj.axon[0].connect(hobj.soma[0], 0.5, 0) #0.5 means that the connection is made at the midpoint of the soma
+
+    #Define the shape of the axon
+    h.define_shape()
+
+    return hobj
+
+
+def aibs_perisomatic(hobj, cell, dynamics_params):
+    if dynamics_params is not None:
+        node_id = cell["node_id"]
+        cell_type = cell['pop_name']       
+        io.log_info(f'Fixing cell #{node_id}, {cell_type}')
+        
+        # fix_axon_peri(hobj)
+        fix_axon_peri_multiple_stubs(hobj, 4, [30,30,30,30], [1,1,1,1])
+        set_params_peri(hobj, dynamics_params)
+
+    return hobj
+
+
+add_cell_processor(aibs_perisomatic, overwrite=True)
+
+
+conf = bionet.Config.from_json('simulation/config.json')
+#conf=bionet.Config.from_json('simulation_long_axons/config.json')
+#conf=bionet.Config.from_json(simulation_directed_axons/config.json')
+conf.build_env()
+net = bionet.BioNetwork.from_config(conf)
+sim = bionet.BioSimulator.from_config(conf, network=net)
+sim.run()
