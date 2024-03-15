1237216fcfa8168342184052ab67f29090c911c3
ankevge <anke.verhaege@gmail.com>
2024-03-15 11:10:24 +0100

    add title

--- v1_Anke/small_population_exp/analyze_run_small_population.py
+++ v1_Anke/small_population_exp/analyze_run_small_population.py
@@ -1,3 +1,3 @@
 from bmtk.analyzer.spike_trains import plot_raster
 
-plot_raster(config_file = 'simulation/config.json')
+plot_raster(config_file = 'simulation/config.json', title='basic simulation (without axons)')
