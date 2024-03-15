4662cb469b4b57901adf8b59f8d1c9753b995c51
ankevge <anke.verhaege@gmail.com>
2024-03-15 12:19:42 +0100

    analyze file

--- v1_Anke/small_population_exp/analyze_run_small_population.py
+++ v1_Anke/small_population_exp/analyze_run_small_population.py
@@ -2,6 +2,7 @@ from bmtk.analyzer.spike_trains import plot_raster
 
 number_stubs='10'
 diam='5'
-title_plot='simulation 10 axon stubs, diam 5'
+title_plot='simulation 10 axon stubs'
 
-plot_raster(config_file = 'simulation_long_'+number_stubs+'_diam_'+diam+'/config.json', spikes_file='simulation_long_'+number_stubs+'_diam_'+diam+'/output/spikes.csv', title=title_plot)
+plot_raster(config_file = 'simulation_long_10/config.json', spikes_file= 'simulation_long_10/output/spikes.csv', title=title_plot)
+#plot_raster(config_file = 'simulation_long_'+number_stubs+'_diam_'+diam+'/config.json', spikes_file='simulation_long_'+number_stubs+'_diam_'+diam+'/output/spikes.csv', title=title_plot)
