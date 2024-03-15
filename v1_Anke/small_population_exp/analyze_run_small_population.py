6049f2fe4e4a637726b4289c8438eb1704d7742e
ankevge <anke.verhaege@gmail.com>
2024-03-15 12:23:47 +0100

    minor analyze adjust

--- v1_Anke/small_population_exp/analyze_run_small_population.py
+++ v1_Anke/small_population_exp/analyze_run_small_population.py
@@ -2,7 +2,7 @@ from bmtk.analyzer.spike_trains import plot_raster
 
 number_stubs='10'
 diam='5'
-title_plot='simulation 10 axon stubs'
+title_plot='simulation no axons'
 
-plot_raster(config_file = 'simulation_long_10/config.json', spikes_file= 'simulation_long_10/output/spikes.csv', title=title_plot)
+plot_raster(config_file = 'simulation/config.json', spikes_file= 'simulation/output/spikes.csv', title=title_plot)
 #plot_raster(config_file = 'simulation_long_'+number_stubs+'_diam_'+diam+'/config.json', spikes_file='simulation_long_'+number_stubs+'_diam_'+diam+'/output/spikes.csv', title=title_plot)
