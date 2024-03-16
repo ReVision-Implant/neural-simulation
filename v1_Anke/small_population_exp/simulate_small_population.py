8d84f97cd62feee95b256209b024eeeecb4c8028
ankevge <anke.verhaege@gmail.com>
2024-03-16 14:55:45 +0100

    preparations basic simulation dense network

--- v1_Anke/small_population_exp/simulate_small_population.py
+++ v1_Anke/small_population_exp/simulate_small_population.py
@@ -6,10 +6,10 @@ from bmtk.simulator import bionet
 
 
 build_env_bionet(
-    base_dir='simulation_test',
+    base_dir='simulation_basic',
     #base_dir='simulation_long_4',
     config_file='config.json',
-    network_dir='network_mask',
+    network_dir='network_mask_dense',
     tstop=100.0, dt=0.025,
     #report_vars=['v'], # Record membrane potential
     #current_clamp={  # Creates a step current from 500.0 ms to 1500.0 ms  
