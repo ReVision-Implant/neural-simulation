bd56aaa8f43573da8eb0248450c25fe4728fad53
ankevge <anke.verhaege@gmail.com>
2024-03-21 11:09:16 +0100

    set up test simulation different electrodes

--- v1_Anke/small_population_exp/simulate_small_population.py
+++ v1_Anke/small_population_exp/simulate_small_population.py
@@ -6,12 +6,12 @@ from bmtk.simulator import bionet
 
 
 build_env_bionet(
-    base_dir='simulation_axon_10_diam_5',
+    base_dir='simulation_basic_el_1_5',
     #base_dir='simulation_long_4',
     config_file='config.json',
     network_dir='network_mask_dense',
     tstop=100.0, dt=0.025,
-    report_vars=['v'], # Record membrane potential
+    #report_vars=['v'], # Record membrane potential
     #current_clamp={  # Creates a step current from 500.0 ms to 1500.0 ms  
      #   'amp': 0.120,
     #    'delay': 500.0,
