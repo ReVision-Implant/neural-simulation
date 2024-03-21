ad19f8821da0aa1c3700f19eacfb9216704e0e4c
ankevge <anke.verhaege@gmail.com>
2024-03-21 16:20:32 +0100

    prepare simulation both far

--- v1_Anke/2_neuron_exp/simulate_2_cell.py
+++ v1_Anke/2_neuron_exp/simulate_2_cell.py
@@ -1,9 +1,9 @@
 from bmtk.utils.sim_setup import build_env_bionet
 
 build_env_bionet(
-    base_dir='simulation_basic',
+    base_dir='simulation_basic_both_far',
     config_file='config.json',
-    network_dir='2_neuron_net_full_connect',
+    network_dir='both_far',
     tstop=100.0, dt=0.025,
     report_vars=['v'], # Record membrane potential
 )
