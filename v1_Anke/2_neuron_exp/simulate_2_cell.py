67d8485b5d54960cb93d59f4bfcc7a9bad520b88
ankevge <anke.verhaege@gmail.com>
2024-03-22 10:49:27 +0100

    build and run close y

--- v1_Anke/2_neuron_exp/simulate_2_cell.py
+++ v1_Anke/2_neuron_exp/simulate_2_cell.py
@@ -1,9 +1,9 @@
 from bmtk.utils.sim_setup import build_env_bionet
 
 build_env_bionet(
-    base_dir='sim_basic_1_close_x',
+    base_dir='sim_basic_1_close_y',
     config_file='config.json',
-    network_dir='networks/1_close_x',
+    network_dir='networks/1_close_y',
     tstop=100.0, dt=0.025,
     report_vars=['v'], # Record membrane potential
 )
