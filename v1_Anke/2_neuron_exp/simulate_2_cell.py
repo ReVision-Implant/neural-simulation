b1387ef49729fb2a7ec126e53d52bef62d60179c
ankevge <anke.verhaege@gmail.com>
2024-03-22 11:12:57 +0100

    sim basic far z

--- v1_Anke/2_neuron_exp/simulate_2_cell.py
+++ v1_Anke/2_neuron_exp/simulate_2_cell.py
@@ -1,9 +1,9 @@
 from bmtk.utils.sim_setup import build_env_bionet
 
 build_env_bionet(
-    base_dir='sim_axon_10_diam_1_begin_both_far_z_',
+    base_dir='sim_basic/sim_basic_far_z',
     config_file='config.json',
-    network_dir='networks/both_far_z',
+    network_dir='networks/far_z',
     tstop=100.0, dt=0.025,
     report_vars=['v'], # Record membrane potential
 )
