b4a07dd3ef91fca84596619f2e37f52279975ef8
ankevge <anke.verhaege@gmail.com>
2024-03-22 10:53:50 +0100

    rename

--- v1_Anke/2_neuron_exp/simulate_2_cell.py
+++ v1_Anke/2_neuron_exp/simulate_2_cell.py
@@ -1,9 +1,9 @@
 from bmtk.utils.sim_setup import build_env_bionet
 
 build_env_bionet(
-    base_dir='sim_basic_1_close_y',
+    base_dir='sim_basic_both_far_z',
     config_file='config.json',
-    network_dir='networks/1_close_y',
+    network_dir='networks/1_both_far_z',
     tstop=100.0, dt=0.025,
     report_vars=['v'], # Record membrane potential
 )
