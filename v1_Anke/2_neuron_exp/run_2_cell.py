4e6aea1f4a5bad931ec497d4cb8eaa55c8b111ab
ankevge <anke.verhaege@gmail.com>
2024-03-26 11:01:54 +0100

    run stimulation

--- v1_Anke/2_neuron_exp/run_2_cell.py
+++ v1_Anke/2_neuron_exp/run_2_cell.py
@@ -105,11 +105,11 @@ def aibs_perisomatic(hobj, cell, dynamics_params):
     return hobj
 
 
-add_cell_processor(aibs_perisomatic, overwrite=True)
+#add_cell_processor(aibs_perisomatic, overwrite=True)
 
 
 #conf = bionet.Config.from_json('simulation/config.json')
-conf=bionet.Config.from_json('sim_axon_10_diam_1/amplitude_20/conduct_hh/config.json')
+conf=bionet.Config.from_json('sim_waveform_with_pause_2s/sim_basic/amplitude_10/sim_close_z/config.json')
 conf.build_env()
 net = bionet.BioNetwork.from_config(conf)
 sim = bionet.BioSimulator.from_config(conf, network=net)
