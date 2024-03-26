b4523771000623847133d778926d801b491f2a85
ankevge <anke.verhaege@gmail.com>
2024-03-26 11:57:52 +0100

    sim and run 20 amp

--- v1_Anke/2_neuron_exp/simulate_2_cell.py
+++ v1_Anke/2_neuron_exp/simulate_2_cell.py
@@ -1,9 +1,9 @@
 from bmtk.utils.sim_setup import build_env_bionet
 
 build_env_bionet(
-    base_dir='sim_waveform_5ms_pause/sim_basic/amplitude_10/sim_close_x',
+    base_dir='sim_waveform_5ms_pause/sim_basic/amplitude_20/sim_close_z',
     config_file='config.json',
-    network_dir='networks/1_close_x',
+    network_dir='networks/1_close',
     tstop=3000.0, dt=0.025,
     report_vars=['v'], # Record membrane potential
 )
