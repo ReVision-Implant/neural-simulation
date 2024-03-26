0ea0819fd7892bd37cd2a6c73ef72e1857cd58fa
ankevge <anke.verhaege@gmail.com>
2024-03-26 11:20:04 +0100

    new waveform 5 ms pause

--- v1_Anke/2_neuron_exp/simulate_2_cell.py
+++ v1_Anke/2_neuron_exp/simulate_2_cell.py
@@ -3,7 +3,7 @@ from bmtk.utils.sim_setup import build_env_bionet
 build_env_bionet(
     base_dir='sim_waveform_with_pause_2s/sim_basic/amplitude_10/sim_close_z',
     config_file='config.json',
-    network_dir='networks/far_z',
-    tstop=100.0, dt=0.025,
+    network_dir='networks/1_close',
+    tstop=3000.0, dt=0.025,
     report_vars=['v'], # Record membrane potential
 )
--- v1_Anke/toolbox/waveform_start_pause.py
+++ v1_Anke/toolbox/waveform_start_pause.py
@@ -169,12 +169,12 @@ if __name__ == '__main__':
 
     CreateBlockWaveform(
         n_pulses = 20,
-        pause_duration=2000,
+        pause_duration=5,
         phase_1_expr = lambda n:0.2,
         amp_1_expr = lambda n:-1,
         T_1_expr = lambda n:0,
         phase_2_expr = lambda n:0.2,
         amp_2_expr = lambda n:1,
         T_2_expr = lambda n:4.6,
-        save_name = "/components/stimulation/waveforms/waveform_pause_2s.csv"
+        save_name = "/components/stimulation/waveforms/waveform_pause_20ms.csv"
     )
