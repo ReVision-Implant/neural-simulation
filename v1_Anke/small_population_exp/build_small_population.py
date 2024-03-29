c7111e1c19f3b760b95b76068e1756e6e69ec1b6
ankevge <anke.verhaege@gmail.com>
2024-03-29 15:27:45 +0100

    build test network connection 0

--- v1_Anke/small_population_exp/build_small_population.py
+++ v1_Anke/small_population_exp/build_small_population.py
@@ -36,12 +36,20 @@ net.add_nodes(
     morphology="Cux2-CreERT2_Ai14-211772.05.02.01_496085150_m.swc"
 )
 
+def self_connect(source, target):
+    if source['node_id'] == target['node_id']:
+        return 1
+    else:
+        return None
+
 
 net.add_edges(
     source={'pop_name': 'e23Cux2'}, target={'pop_name': 'e23Cux2'},
     connection_rule=distance_connector, #differs to the bigger model; connections based on probability based on distance
+    #connection_rule=self_connect,
     connection_params={'d_weight_min': 0.0, 'd_weight_max': 0.34, 'd_max': 50.0, 'nsyn_min': 0, 'nsyn_max': 10},
-    syn_weight=0.000182520075015,
+    #syn_weight=0.000182520075015,
+    syn_weight = 0,
     distance_range=[0.0, 200.0],
     target_sections=['basal', 'apical'],
     delay=1.6,
@@ -50,5 +58,5 @@ net.add_edges(
 )
 
 net.build()
-net.save_nodes(output_dir='network_mask_dense')
-net.save_edges(output_dir='network_mask_dense')
+net.save_nodes(output_dir='network_no_connections_test')
+net.save_edges(output_dir='network_no_connections_test')
