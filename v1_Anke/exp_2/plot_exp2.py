6faf3bf9a27708a0e54fb1cb1218fb448425708b
ankevge <anke.verhaege@student.kuleuven.be>
2024-03-29 11:41:14 +0100

    plot function

--- v1_Anke/exp_2/plot_exp2.py
+++ v1_Anke/exp_2/plot_exp2.py
@@ -1,14 +1,17 @@
 import sys
 module_path='/scratch/leuven/356/vsc35693/neural-simulation/v1_Anke/toolbox';
 sys.path.append(module_path)
-from plotter_helper import plot_all
+from plotter_helper import Plotter
 
 exp = [2]
 patterns = [0,4,5,6,7,8]
-amplitudes[10]
-mice=[0,1,2]
+amplitudes=[10,20]
+mice=[0,1]
+n_rows=6
+n_cols=2
 row_param ="patterns"
 col_param ="mice"
 
+plot=Plotter(n_rows,n_cols)
 
-plot_all(self, exp, patterns, amplitudes, mice, row_param=None, col_param=None, depth=None, sigma=10, centroid=True, electrodes=True, ticks=True)
+plot.plot_all(exp, patterns, amplitudes, mice, row_param, col_param, depth=None, sigma=10, centroid=True, electrodes=True, ticks=True)
