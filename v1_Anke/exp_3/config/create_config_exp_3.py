b6d953d8787d298b320ab1256a49b0bb17a3abf5
ankevge <anke.verhaege@student.kuleuven.be>
2024-04-01 16:16:31 +0200

    config files exp 3 amplitude

--- v1_Anke/exp_3/config/create_config_exp_3.py
+++ v1_Anke/exp_3/config/create_config_exp_3.py
@@ -4,5 +4,5 @@ sys.path.append(module_path);
 
 from file_helper import create_configs;
 
-create_configs('./template.json',3, [0,4,5,6,7,8], [10], [3,4,5], overwrite=True);
+create_configs('./template.json',3, [0], [20], [3,4,5], overwrite=True);
 
