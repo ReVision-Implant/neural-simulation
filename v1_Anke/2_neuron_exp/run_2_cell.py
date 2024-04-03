1efeda04487a97799d81fceb207100154e41641a
ankevge <anke.verhaege@gmail.com>
2024-04-03 12:27:12 +0200

    get axon direction function added

--- v1_Anke/2_neuron_exp/run_2_cell.py
+++ v1_Anke/2_neuron_exp/run_2_cell.py
@@ -6,9 +6,15 @@ from bmtk.simulator import bionet
 from bmtk.simulator.bionet.pyfunction_cache import add_cell_processor
 from bmtk.simulator.bionet.default_setters.cell_models import fix_axon_peri, set_params_peri
 from bmtk.simulator.bionet.io_tools import io
+from bmtk.simulator.bionet.nml_reader import NMLTree
 from neuron import h
 from ipdb import set_trace
-
+import os
+import numpy as np
+try:
+    from sklearn.decomposition import PCA
+except Exception as e:
+    pass
 
 def fix_axon_peri_multiple_stubs(hobj, num_stubs, stub_lengths, stub_diameters):
     """
@@ -195,6 +201,51 @@ def set_params_peri_active_axon(hobj, biophys_params):
             sec.ena = erev["ena"]
             sec.ek = erev["ek"]
 
+def get_axon_direction(hobj):
+    for sec in hobj.somatic:
+        n3d = int(h.n3d(sec=sec))  # get number of n3d points in each section
+        soma_end = np.asarray([h.x3d(n3d - 1, sec=sec), h.y3d(n3d - 1, sec=sec), h.z3d(n3d - 1, sec=sec)])
+        mid_point = int(n3d / 2)
+        soma_mid = np.asarray([h.x3d(mid_point, sec=sec), h.y3d(mid_point, sec=sec), h.z3d(mid_point, sec=sec)])
+
+    for sec in hobj.all:
+        section_name = sec.name().split(".")[1][:4]
+        if section_name == 'axon':
+            n3d = int(h.n3d(sec=sec))  # get number of n3d points in each section
+            axon_p3d = np.zeros((n3d, 3))  # to hold locations of 3D morphology for the current section
+            for i in range(n3d):
+                axon_p3d[i, 0] = h.x3d(i, sec=sec)
+                axon_p3d[i, 1] = h.y3d(i, sec=sec)  # shift coordinates such to place soma at the origin.
+                axon_p3d[i, 2] = h.z3d(i, sec=sec)
+
+    # Add soma coordinates to the list
+    p3d = np.concatenate(([soma_mid], axon_p3d), axis=0)
+
+    # Compute PCA
+    pca = PCA(n_components=3)
+    pca.fit(p3d)
+    unit_v = pca.components_[0]
+
+    mag_v = np.sqrt(pow(unit_v[0], 2) + pow(unit_v[1], 2) + pow(unit_v[2], 2))
+    unit_v[0] = unit_v[0] / mag_v
+    unit_v[1] = unit_v[1] / mag_v
+    unit_v[2] = unit_v[2] / mag_v
+
+    # Find the direction
+    axon_end = axon_p3d[-1] - soma_mid
+    if np.dot(unit_v, axon_end) < 0:
+        unit_v *= -1
+
+    axon_seg_coor = np.zeros((4, 3))
+    # unit_v = np.asarray([0,1,0])
+    axon_seg_coor[0] = soma_end
+    axon_seg_coor[1] = soma_end + (unit_v * 30.)
+    axon_seg_coor[2] = soma_end + (unit_v * 30.)
+    axon_seg_coor[3] = soma_end + (unit_v * 60.)
+
+    return axon_seg_coor
+
+
 
 def aibs_perisomatic(hobj, cell, dynamics_params):
     if dynamics_params is not None:
@@ -207,10 +258,14 @@ def aibs_perisomatic(hobj, cell, dynamics_params):
         #set_params_peri(hobj, dynamics_params)
         set_params_peri_axon_copy_soma(hobj, dynamics_params)
         #set_params_peri_active_axon(hobj,dynamics_params)
+        axon_seg_coordin = get_axon_direction(hobj)
+        io.log_info(axon_seg_coordin)
 
     return hobj
 
 
+
+#here is the code to edit when just running the simulations, above are all the involved functions
 add_cell_processor(aibs_perisomatic, overwrite=True)
 dir='sim_waveform_5ms_pause/axon_3_diam_1/amplitude_20/copy_soma_close'
 
