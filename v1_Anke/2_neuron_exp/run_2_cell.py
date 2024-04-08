42367052fc3c7b83269b82b8d63332a3757f5746
ankevge <anke.verhaege@gmail.com>
2024-04-08 16:09:31 +0200

    axon stub 2 waveform 4

--- v1_Anke/2_neuron_exp/run_2_cell.py
+++ v1_Anke/2_neuron_exp/run_2_cell.py
@@ -332,9 +332,9 @@ def aibs_perisomatic(hobj, cell, dynamics_params):
         io.log_info(f'Fixing cell #{node_id}, {cell_type}')
         
         #fix_axon_peri(hobj)
-        fix_axon_peri_multiple_stubs(hobj, 4, [30,30,30,30], [1,1,1,1])
-        #set_params_peri(hobj, dynamics_params)
-        set_params_peri_axon_copy_soma(hobj, dynamics_params)
+        fix_axon_peri_multiple_stubs(hobj, 2, [30,30], [1,1])
+        set_params_peri(hobj, dynamics_params)
+        #set_params_peri_axon_copy_soma(hobj, dynamics_params)
         #set_params_peri_active_axon(hobj,dynamics_params)
         #set_params_peri_hh_axon(hobj, dynamics_params)
 
@@ -347,8 +347,8 @@ def aibs_perisomatic(hobj, cell, dynamics_params):
 
 
 #here is the code to edit when just running the simulations, above are all the involved functions
-#add_cell_processor(aibs_perisomatic, overwrite=True)
-dir='sim_axon_4_diam_1/network_1/waveform_4_5ms/amplitude_10/simulation_0'
+add_cell_processor(aibs_perisomatic, overwrite=True)
+dir='sim_axon_2_diam_1/network_1/waveform_4_5ms/amplitude_10/simulation_0'
 
 conf=bionet.Config.from_json(dir+'/config.json')
 conf.build_env()
