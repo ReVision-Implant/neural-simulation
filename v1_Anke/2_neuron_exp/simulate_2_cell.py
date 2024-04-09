51dc9f00330763dca6a7e667b54a1665e8f9872b
ankevge <anke.verhaege@gmail.com>
2024-04-09 12:50:43 +0200

    simulation with waveform 8

--- v1_Anke/2_neuron_exp/simulate_2_cell.py
+++ v1_Anke/2_neuron_exp/simulate_2_cell.py
@@ -1,9 +1,9 @@
 from bmtk.utils.sim_setup import build_env_bionet
 
 build_env_bionet(
-    base_dir='sim_axon_2_diam_1/network_A/waveform_4_5ms/amplitude_10/simulation_0',
+    base_dir='sim_axon_2_diam_1/network_1/waveform_4_-8/amplitude_10/simulation_0',
     config_file='config.json',
-    network_dir='networks/network_A',
+    network_dir='networks/network_1',
     tstop=3000.0, dt=0.025,
     report_vars=['v'], # Record membrane potential
 )
