14765f3603dfd85687c9a72f486a4ab0116e0dd4
ankevge <anke.verhaege@gmail.com>
2024-04-18 18:48:05 +0200

    test

--- v1_Anke/2_neuron_exp/simulate_2_cell.py
+++ v1_Anke/2_neuron_exp/simulate_2_cell.py
@@ -1,9 +1,9 @@
 from bmtk.utils.sim_setup import build_env_bionet
 
 build_env_bionet(
-    base_dir='sim_node_internode/axon_3/waveform_0/amplitude_10/simulation_0',
+    base_dir='sim_internode_tests/no_stim/simulation_0',
     config_file='config.json',
-    network_dir='networks/network_B',
+    network_dir='networks/network_C',
     tstop=3000.0, dt=0.025,
     report_vars=['v'], # Record membrane potential
 )
