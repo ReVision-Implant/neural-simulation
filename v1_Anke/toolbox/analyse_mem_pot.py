9d9b37d4aefa16e89dd3e609d2f689e107e4e881
ankevge <anke.verhaege@student.kuleuven.be>
2024-04-25 16:55:59 +0200

    analyse membrane pot

--- v1_Anke/toolbox/analyse_mem_pot.py
+++ v1_Anke/toolbox/analyse_mem_pot.py
@@ -3,7 +3,7 @@ import sys;
 module_path='/scratch/leuven/356/vsc35693/neural-simulation/';
 sys.path.append(module_path)
 
-dir='/scratch/leuven/356/vsc35693/neural-simulation/v1_Anke/exp_2/config/bkg/bkg_membrane_pot/config.exp2_bkg_vm_m0'
+dir='/scratch/leuven/356/vsc35693/neural-simulation/v1_Anke/exp_1/output/bkg/bkg_membrane_pot/mouse_0/config.exp1_bkg_vm_m0'
 title_plot='background membrane potential v1 model'
 
-plot_traces(config_file=dir+'.json', report_name='v_report', title=title_plot, group_by='layer', save_as= '/scratch/leuven/356/vsc35693/neural-simulation/v1_Anke/exp_2/output/bkg/bkg_membrane_pot/mouse_0/plot_layers.png')
+plot_traces(config_file=dir+'.json', report_name='v_report', title=title_plot, save_as= '/scratch/leuven/356/vsc35693/neural-simulation/v1_Anke/exp_1/output/bkg/bkg_membrane_pot/mouse_0/plot.png')
