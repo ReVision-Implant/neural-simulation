535520ec8c96ca56a18fce7779b5ca5bf7ac6a97
ankevge <anke.verhaege@gmail.com>
2024-04-26 16:59:49 +0200

    delete irrelevant test

--- v1_Anke/single_neuron_synaps/test.py
+++
@@ -1,9 +0,0 @@
-def generate_list(repeat_count):
-    pattern = [30] + [1] * 30
-    result = pattern * repeat_count
-    return result
-
-# Example usage:
-repeat_count = 5
-result_list = generate_list(repeat_count)
-print(result_list)
