1bd88dfea1dba4166c42165c0a96c8ad51c43810
ankevge <anke.verhaege@gmail.com>
2024-04-27 12:19:52 +0200

    prepare simulation 7

--- v1_Anke/single_neuron_synaps/simulator.py
+++ v1_Anke/single_neuron_synaps/simulator.py
@@ -1,7 +1,7 @@
 from bmtk.utils.sim_setup import build_env_bionet
 
 build_env_bionet(
-    base_dir='simulation_6',
+    base_dir='simulation_7',
     config_file='config.json',
     network_dir='networks/network_1',
     tstop=3000.0, dt=0.1,
