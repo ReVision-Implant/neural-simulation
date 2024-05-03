68182cb76e8ac34869173ef2dee74df9207ee530
ankevge <anke.verhaege@student.kuleuven.be>
2024-05-03 11:20:44 +0200

    analyse the average Vm for exp C

--- v1_Anke/toolbox/analyse_mem_pot.py
+++ v1_Anke/toolbox/analyse_mem_pot.py
@@ -3,7 +3,7 @@ import sys;
 module_path='/scratch/leuven/356/vsc35693/neural-simulation/';
 sys.path.append(module_path)
 
-dir='/scratch/leuven/356/vsc35693/neural-simulation/v1_Anke/exp_1/config/bkg/bkg_membrane_pot/config.exp1_bkg_vm_m0_test'
+dir='/scratch/leuven/356/vsc35693/neural-simulation/v1_Anke/exp_1/config/bkg/bkg_membrane_pot/exp_C/config.exp1_bkg_vm_C'
 title_plot='background membrane potential v1 model'
 
-plot_traces(config_file=dir+'.json', report_name='v_report', title=title_plot, save_as= '/scratch/leuven/356/vsc35693/neural-simulation/v1_Anke/exp_1/output/bkg/bkg_membrane_pot/mouse_0_test/plot.png')
+plot_traces(config_file=dir+'.json', report_name='v_report', title=title_plot, save_as= '/scratch/leuven/356/vsc35693/neural-simulation/v1_Anke/exp_1/output/bkg/bkg_membrane_pot/exp_C/plot_vm.png')
