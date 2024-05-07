766e5151f430db338f689a052c124de8cfc52c29
ankevge <anke.verhaege@student.kuleuven.be>
2024-05-07 17:05:11 +0200

    get spikes function works

--- v1_Anke/toolbox/data_analyser.py
+++ v1_Anke/toolbox/data_analyser.py
@@ -3,12 +3,7 @@ from hdf5 import HDF5
 import numpy as np
 import os
 
-
-import numpy as np
-import pandas as pd
-from h5py import File as HDF5
-
-def get_spikes(nodes_dirs, spikes_dirs, spikes_bkg_dirs, **kwargs):
+def get_spikes(nodes_dirs, spikes_dirs, spikes_bkg_dirs, radius=None, depth=None, v1=True, **kwargs):
     """Get spikes and node positions from network and output files.
 
     :param nodes_dirs: directories that point to network/nodes.h5
@@ -19,7 +14,7 @@ def get_spikes(nodes_dirs, spikes_dirs, spikes_bkg_dirs, **kwargs):
     :type spikes_bkg_dirs: path or list thereof
     :param v1: defaults to True.
     :type v1: bool, optional
-    :return: average node positions and average number of spikes for each node
+    :return: node positions
     :rtype: ndarray
     """    
         
@@ -29,15 +24,17 @@ def get_spikes(nodes_dirs, spikes_dirs, spikes_bkg_dirs, **kwargs):
 
     assert len(nodes_dirs) == len(spikes_dirs) == len(spikes_bkg_dirs)
 
-    node_pos_total = np.zeros((0,3)) # Initialize total node positions
-    n_spikes_total = np.zeros((0,1)) # Initialize total spikes count
+    node_pos_list = [] # List to collect node positions
+    n_spikes_list = [] # List to collect n_spikes
 
     for i in range(len(nodes_dirs)):
+
         nodes_dir = nodes_dirs[i]
         spikes_dir = spikes_dirs[i]
         spikes_bkg_dir = spikes_bkg_dirs[i]
 
-        node_pos_temp = HDF5(nodes_dir, v1=True).positions
+        node_pos_temp = HDF5(nodes_dir, v1=v1).positions
+
         n_spikes_temp = np.zeros(np.shape(node_pos_temp)[0])
 
         spikes = pd.read_csv(spikes_dir, sep='\s+')
@@ -46,28 +43,38 @@ def get_spikes(nodes_dirs, spikes_dirs, spikes_bkg_dirs, **kwargs):
                 n_spikes_temp[spikes['node_ids'][ind]] += 1
 
         if spikes_bkg_dirs is not None:
+            spikes_bkg_dir = spikes_bkg_dirs[0] if isinstance(spikes_bkg_dirs, list) else spikes_bkg_dirs 
             spikes_bkg = pd.read_csv(spikes_bkg_dir, sep='\s+')
             for ind in spikes_bkg.index:
                 if spikes_bkg['timestamps'][ind] < 100:
                     n_spikes_temp[spikes_bkg['node_ids'][ind]] = max(0, n_spikes_temp[spikes_bkg['node_ids'][ind]] - 1)
+ 
+        node_pos_list.append(node_pos_temp) # Append node positions to the list
+        n_spikes_list.append(n_spikes_temp) # Append n_spikes to the list
 
-        node_pos_total = np.vstack((node_pos_total, node_pos_temp))
-        n_spikes_total = np.append(n_spikes_total, n_spikes_temp)
+    # Convert lists to numpy arrays
+    node_pos = np.array(node_pos_list)
+    n_spikes = np.array(n_spikes_list)
 
-    # Calculate averages
-    avg_node_pos = node_pos_total.mean(axis=0)
-    avg_n_spikes = n_spikes_total.mean(axis=0)
+    mean_positions = np.mean(node_pos, axis=0)
+    mean_spikes = np.mean(n_spikes, axis = 0)
 
-    return avg_node_pos, avg_n_spikes
-
+    return mean_positions, mean_spikes
 
 path ='/scratch/leuven/356/vsc35693/neural-simulation/v1_Anke'
-node_dirs_0 = [path+'/virtual_mice_mask/mouse_0/v1_nodes.h5',path+'/virtual_mice_mask/mouse_1/v1_nodes.h5']
-spike_dirs_0 = [path+'/exp_2/output/pattern_0/amplitude_10/mouse_0/spikes.csv',path+'/exp_2/output/pattern_0/amplitude_10/mouse_1/spikes.csv']
-spike_bkg_dirs_0= [path+'/exp_2/output/bkg/mouse_0/spikes.csv', path+'/exp_2/output/bkg/mouse_1/spikes.csv']
-node_positions_pattern_0_amp_10, n_spikes_pattern_0_amp_10 = get_spikes(nodes_dirs = node_dirs_0, spikes_dirs = spike_dirs_0, spikes_bkg_dirs = spike_bkg_dirs_0)
+node_dirs_A = [path+'/virtual_mice_mask/mouse_0/v1_nodes.h5', path+'/virtual_mice_mask/mouse_1/v1_nodes.h5']
+spike_dirs_A = [path+'/exp_2/output/pattern_0/amplitude_10/mouse_0/spikes.csv', path+'/exp_2/output/pattern_0/amplitude_10/mouse_1/spikes.csv']
+spike_bkg_dirs_A= [path+'/exp_2/output/bkg/mouse_0/spikes.csv',path+'/exp_2/output/bkg/mouse_1/spikes.csv']
+node_pos_A, n_spikes_A = get_spikes(nodes_dirs = node_dirs_A, spikes_dirs = spike_dirs_A, spikes_bkg_dirs = spike_bkg_dirs_A)
 
-print(node_positions_pattern_0_amp_10[147996])
-print(n_spikes_pattern_0_amp_10[147996])
-max_value = np.max(n_spikes_pattern_0_amp_10)
-#print(max_value)
+node_dirs_B = [path+'/virtual_mice_mask/mouse_0/v1_nodes.h5', path+'/virtual_mice_mask/mouse_1/v1_nodes.h5']
+spike_dirs_B = [path+'/exp_2/output/pattern_4/amplitude_10/mouse_0/spikes.csv', path+'/exp_2/output/pattern_4/amplitude_10/mouse_1/spikes.csv']
+spike_bkg_dirs_B= [path+'/exp_2/output/bkg/mouse_0/spikes.csv',path+'/exp_2/output/bkg/mouse_1/spikes.csv']
+node_pos_B, n_spikes_B = get_spikes(nodes_dirs = node_dirs_B, spikes_dirs = spike_dirs_B, spikes_bkg_dirs = spike_bkg_dirs_B)
+
+#print(n_spikes_A.shape)
+#print(n_spikes_B.shape)
+#print(np.max(n_spikes_A))
+#print(np.max(n_spikes_B))
+#print(np.mean(n_spikes_A, axis =0))
+#print(np.mean(n_spikes_B, axis =0))
