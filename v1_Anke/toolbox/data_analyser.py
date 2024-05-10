fc8dfcd469a1a3aa433ba8bc0d335d4509ba5014
ankevge <anke.verhaege@student.kuleuven.be>
2024-05-10 17:45:29 +0200

    plots work

--- v1_Anke/toolbox/data_analyser.py
+++ v1_Anke/toolbox/data_analyser.py
@@ -68,7 +68,18 @@ def discriminate_signed_rank(n_spikes_A, n_spikes_B,pattern_A,pattern_B):
         '''
         Use the Wilcoxon signed-rank test to get a p-value as index of separability between the two neuronal populations.
         '''
-        signed_rank = wilcoxon(n_spikes_A, n_spikes_B) # Apply Wilcoxon test
+        n_spikes_A_filtered=[]
+        n_spikes_B_filtered=[]
+        for value1, value2 in zip(n_spikes_A, n_spikes_B):
+            if value1 >= 5 or value2 >=5:
+                #print(value1,value2)
+                n_spikes_A_filtered.append(value1)
+                n_spikes_B_filtered.append(value2)
+
+        n_spikes_A= n_spikes_A_filtered[0:100]
+        n_spikes_B= n_spikes_B_filtered[0:100]
+        
+        signed_rank = wilcoxon(n_spikes_A, n_spikes_B, zero_method="zsplit") # Apply Wilcoxon test
         print('P-value for Wilcoxon signed-rank test for stim patterns '+str(pattern_A)+' and '+str(pattern_B)+' is ' + str(round(signed_rank.pvalue,5)))
         return(signed_rank.pvalue)
 
@@ -81,7 +92,7 @@ def kernel_density_estimate(node_pos, n_spikes, pattern):
         coordinates = coordinates[non_zero_indices]
         n_spikes= n_spikes[non_zero_indices]
 
-        kde = KernelDensity(bandwidth=10, kernel='gaussian') # Choose model and parameters
+        kde = KernelDensity(bandwidth=50, kernel='gaussian') # Choose model and parameters
         ###vanaf hier verder werken
         kde.fit(coordinates, sample_weight=n_spikes) # Train model
 
@@ -98,14 +109,14 @@ def kernel_density_estimate(node_pos, n_spikes, pattern):
         plt.ylabel('Y Coordinate')
         plt.xlim([-250, 400])
         plt.ylim([100, 800])
-        #plt.gca().invert_yaxis()  # Invert y-axis for better comparison with ImageJ
+        plt.gca().invert_yaxis()  # Invert y-axis for better comparison with ImageJ
         plt.gca().set_aspect('equal', adjustable='box')  # Set aspect ratio to be equal
         # plt.legend()
         plt.title('Kernel Density Estimate for stim. pattern ' + str(pattern))
-        plt.show()
-        #plt.close()
+        #plt.show()
+        plt.close()
 
-        return coordinates, n_spikes, y_grid, z_grid, density
+        return y_grid, z_grid, density
 
 def projected_kernel_density_estimate(node_pos,n_spikes):
         '''
@@ -126,9 +137,11 @@ def projected_kernel_density_estimate(node_pos,n_spikes):
         grid_size = 100
         grid_z = np.linspace(min(projected_points_z), max(projected_points_z), grid_size).reshape(-1, 1)
         grid_y = np.linspace(min(projected_points_y), max(projected_points_y), grid_size).reshape(-1, 1)
+        #print("projected points min and max z", min(projected_points_z), max(projected_points_z))
+        #print("projected points min and max y", min(projected_points_y), max(projected_points_y))
 
         # Perform kernel density estimation
-        kde = KernelDensity(bandwidth=5, kernel='gaussian') 
+        kde = KernelDensity(bandwidth=50, kernel='gaussian') 
 
         kde_z=kde.fit(projected_points_z.reshape(-1, 1), sample_weight=n_spikes)
         density_z = np.exp(kde_z.score_samples(grid_z))
@@ -148,10 +161,83 @@ def projected_kernel_density_estimate(node_pos,n_spikes):
         ax[1].set_ylabel('Density')
         ax[1].set_title('1D Kernel Density Estimate along y axis')
         plt.tight_layout()
-        plt.show()
-        #plt.close()
+        #plt.show()
+        plt.close()
 
         return grid_y, grid_z, density_y, density_z
+
+def full_kde(node_pos, n_spikes, pattern):
+    grid_y_2D,grid_z_2D, density_2D=kernel_density_estimate(node_pos, n_spikes, pattern)
+    grid_y, grid_z, density_y, density_z = projected_kernel_density_estimate(node_pos, n_spikes)
+    max_y_axis=grid_y[np.argmax(density_y)][0]
+    max_z_axis=grid_z[np.argmax(density_z)][0]
+
+    non_zero_indices = np.nonzero(n_spikes)
+    coordinates= node_pos[:,1:] #select only the y and z coordinates
+    coordinates = coordinates[non_zero_indices]
+    n_spikes= n_spikes[non_zero_indices]
+
+    max_spikes=np.max(n_spikes)
+    n_spikes_norm=n_spikes/max_spikes
+
+    electrode_0_zy=[16,300]
+    electrode_1_zy=[198,300]
+    electrode_2_zy=[16,170]
+    electrode_3_zy=[198,170]
+
+    fig = plt.figure(figsize=(8,12))
+
+    ax1 = plt.subplot2grid((4, 2), (0, 0), colspan=1, rowspan=2)  # 1st row, 1st column, spanning 1 column
+    ax2 = plt.subplot2grid((4, 2), (0, 1), colspan=1, rowspan=2)  # 1st row, 2nd column, spanning 1 column
+    ax3 = plt.subplot2grid((4, 2), (2, 0), colspan=2)  # 2nd row, 1st column, spanning 2 columns
+    ax4 = plt.subplot2grid((4, 2), (3, 0), colspan=2)  # 3rd row, 1st column, spanning 2 columns
+        
+    ax1.axline(electrode_0_zy, electrode_1_zy, color='limegreen', label='Along layer')
+    ax1.axline(electrode_0_zy, electrode_2_zy, color='darkgreen', label='Along column')
+    ax1.scatter(coordinates[:,1], coordinates[:,0], s=90, c="blue", alpha=n_spikes_norm)
+    ax1.scatter(electrode_0_zy[0], electrode_0_zy[1], color='orange', s=110, marker='s', label='Central electrode', zorder=3)
+    ax1.scatter(electrode_1_zy[0], electrode_1_zy[1], color='gold', s=110, marker='s', label='Return electrode in L4', zorder=3)
+    #ax1.scatter(electrode_2_zy[0], electrode_2_zy[1], color='gold', s=110, marker='s', label='Return electrode in L2/3', zorder=3)
+    #ax1.scatter(electrode_2_zy[0], electrode_2_zy[1], color='gold', s=110, marker='s', label='Return electrode in L2/3', zorder=3)
+    ax1.scatter(max_z_axis, electrode_0_zy[1], color='red', marker='*', s=120, label='Max density', zorder=3)
+    ax1.scatter(electrode_0_zy[0], max_y_axis, color='red', marker='*', s=120, zorder=3)
+    ax1.scatter(max_z_axis,max_y_axis, color='red', marker='*', s=120, zorder=3)
+
+    ax1.set_xlabel('Z Coordinate')
+    ax1.set_ylabel('Y Coordinate')
+    ax1.set_xlim([-250,500])
+    ax1.set_ylim([100, 800])
+    ax1.invert_yaxis()  # Invert y-axis for better comparison with ImageJ
+    ax1.set_aspect('equal', adjustable='box')  # Set aspect ratio to be equal
+    ax1.legend(fontsize='8', loc='lower right')
+
+    pcm = ax2.pcolormesh(grid_z_2D, grid_y_2D, density_2D, shading='auto')
+    ax2.scatter(coordinates[:,1], coordinates[:,0], c=n_spikes, cmap='viridis', edgecolors='k', linewidths=1)
+    fig.colorbar(pcm, ax=ax2, label='Values')
+    ax2.set_xlabel('Z Coordinate')
+    ax2.set_ylabel('Y Coordinate')
+    ax2.set_xlim([-250, 500])
+    ax2.set_ylim([100, 800])
+    ax2.invert_yaxis()  # Invert y-axis for better comparison with ImageJ
+    ax2.set_aspect('equal', adjustable='box')  # Set aspect ratio to be equal
+    ax2.set_title('2D Kernel Density Estimate')
+
+    ax3.plot(grid_z, density_z, color='red', linestyle='-')
+    ax3.set_xlabel('Distance along the z axis')
+    ax3.set_ylabel('Density')
+    ax3.set_title('1D Kernel Density Estimate along z axis')
+
+    ax4.plot(grid_y, density_y, color='red', linestyle='-')
+    ax4.set_xlabel('Distance along the y axis')
+    ax4.set_ylabel('Density')
+    ax4.set_title('1D Kernel Density Estimate along y axis')
+
+
+    fig.suptitle('Kernel Density Estimate for stimulation pattern ' + str(pattern))
+    plt.tight_layout(h_pad=4)
+    plt.show()
+
+    return max_y_axis, max_z_axis  
 
 path ='/scratch/leuven/356/vsc35693/neural-simulation/v1_Anke'
 exp=2
@@ -165,8 +251,8 @@ node_pos_B, n_spikes_B = get_spikes(exp=exp,pattern=pattern_B,mouse=mouse,amplit
 
 #p_value_wilcoxon = discriminate_signed_rank(n_spikes_A= n_spikes_A, n_spikes_B=n_spikes_B, pattern_A=0, pattern_B=4)
 #coordin_A, n_spikes_A, y_grid_A, z_grid_A, density_A = kernel_density_estimate(node_pos=node_pos_A,n_spikes=n_spikes_A, pattern=pattern_A)
-grid_y_A, grid_z_A, density_y_A, density_z_A = projected_kernel_density_estimate(node_pos_A, n_spikes_A)
-
+#grid_y_A, grid_z_A, density_y_A, density_z_A = projected_kernel_density_estimate(node_pos_A, n_spikes_A)
+max_y,max_z = full_kde(node_pos_A, n_spikes_A,pattern_A)
 #Underneath: test_code
 
 #coordinates= node_pos_A[:,1:]
@@ -199,14 +285,16 @@ plt.show()'''
 plt.title("plot spiking neurons")
 plt.show()'''
 
-#p_value_test = discriminate_signed_rank(n_spikes_A=[0,2,3,0], n_spikes_B=[0,1,3,0])
-#print(p_value_test)
 #print(p_value_wilcoxon)
+#p_value_test = discriminate_signed_rank(n_spikes_A=[0,2,3,0], n_spikes_B=[0,1,3,0], pattern_A=0, pattern_B=4)
+#print(p_value_test)
 #print(n_spikes_A[149472])
 #print(n_spikes_B[149472])
 #print(n_spikes_A.shape)
 #print(node_pos_A.shape)
 #print(n_spikes_B.shape)
+#print(n_spikes_A)
+#print(n_spikes_B)
 #print(node_pos_B.shape)
 #print(np.max(n_spikes_A))
 #print(np.max(n_spikes_B))
