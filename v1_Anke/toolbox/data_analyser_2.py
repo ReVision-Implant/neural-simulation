828cc5981b2b6c481763e46b82ad2eee9e0b97b0
ankevge <anke.verhaege@gmail.com>
2024-09-17 12:01:51 +0200

    test

--- v1_Anke/toolbox/data_analyser_2.py
+++ v1_Anke/toolbox/data_analyser_2.py
@@ -156,7 +156,7 @@ def Spearmancorr(n_spikes_A, n_spikes_B,pattern_A,pattern_B,threshold_A, thresho
         plt.xlabel('Spike rates for pattern ' + str(pattern_A))
         plt.ylabel('Spike rates for pattern ' + str(pattern_B))
         plt.title("Correlation of patterns "+str(pattern_A)+" and "+str(pattern_B))
-        plt.savefig("/scratch/leuven/356/vsc35693/neural-simulation/v1_Anke/exp_4/plots_spearman/correlation_p"+str(pattern_A)+"_and_p_"+str(pattern_B)+".png")
+        #plt.savefig("/scratch/leuven/356/vsc35693/neural-simulation/v1_Anke/exp_4/plots_spearman/correlation_p"+str(pattern_A)+"_and_p_"+str(pattern_B)+".png")
         plt.show()
         
     
@@ -410,11 +410,11 @@ positions_filtered_B, spikes_filtered_B, threshold_B = filter_spikes(node_pos_B,
 #        max_y_axis_1, max_z_axis_1 = plot1_kde(positions_filtered_1, spikes_filtered_1, pattern_1, mouse_1,amplitude_1)
 #        #max_y_1,max_z_1 = full_kde(positions_filtered_1, spikes_filtered_1, pattern_1,mouse_1,amplitude_1)
 
-#spearman, pvalue = Spearmancorr(n_spikes_A= n_spikes_A, n_spikes_B=n_spikes_B, pattern_A=pattern_A, pattern_B=pattern_B, threshold_A = threshold_A, threshold_B = threshold_B)
-#statistic, pvalue = Pearsoncorrel(n_spikes_A= n_spikes_A, n_spikes_B=n_spikes_B, pattern_A=pattern_A, pattern_B=pattern_B, threshold_A = threshold_A, threshold_B = threshold_B)
+spearman, pvalue = Spearmancorr(n_spikes_A= n_spikes_A, n_spikes_B=n_spikes_B, pattern_A=pattern_A, pattern_B=pattern_B, threshold_A = threshold_A, threshold_B = threshold_B)
+statistic, pvalue = Pearsoncorrel(n_spikes_A= n_spikes_A, n_spikes_B=n_spikes_B, pattern_A=pattern_A, pattern_B=pattern_B, threshold_A = threshold_A, threshold_B = threshold_B)
 #coordin_A, n_spikes_A, y_grid_A, z_grid_A, density_A = kernel_density_estimate(node_pos=node_pos_A,n_spikes=n_spikes_A, pattern=pattern_A)
 #grid_y_A, grid_z_A, density_y_A, density_z_A = projected_kernel_density_estimate(node_pos_A, n_spikes_A)
-max_y_A,max_z_A = full_kde(positions_filtered_A, spikes_filtered_A, pattern_A,mouse_A,amplitude_A)
+#max_y_A,max_z_A = full_kde(positions_filtered_A, spikes_filtered_A, pattern_A,mouse_A,amplitude_A)
 
 #Underneath: test_code
 
