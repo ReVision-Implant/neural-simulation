289a7148be673f33ad08b9669611b6799b3288d4
ankevge <anke.verhaege@gmail.com>
2026-06-26 11:09:25 +0100

    new config files

--- v1_Anke/exp_6/config/create_config_exp_6.py
+++ v1_Anke/exp_6/config/create_config_exp_6.py
@@ -1,8 +1,9 @@
 import sys;
-module_path='/scratch/leuven/356/vsc35693/neural-simulation/v1_Anke/toolbox';
+#module_path='/scratch/leuven/356/vsc35693/neural-simulation/v1_Anke/toolbox';
+module_path=module_path = r"C:\Users\ankev\OneDrive\Documenten\Github\ReVision\neural-simulation\v1_Anke\toolbox"
 sys.path.append(module_path);
 
 from file_helper import create_configs;
 
-create_configs('./template.json',6, [0,1,2,3,4,5,6,7,8,9],[10,20],[0,1,2], overwrite=True);
+create_configs('./template.json',6, [0,1,2,3],[10,20],[0,1,2], overwrite=True);
 
